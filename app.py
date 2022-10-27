08a7a65644049d8d21ec6bc33d20c48952143f2e
elaineads <elaineaps@outlook.com.br>
2022-10-27 18:27:58 -0300

    menu e home versão mobile, adição de cabeçalho (mobile)

--- app.py
+++ app.py
@@ -350,7 +350,7 @@ def home():
 #Rotas Transferencia, Extrato, Configurações inicializadas. Mas ainda sem função.
 @app.route("/transferencia", methods=["GET", "POST"])
 def transferencia():
-    return render_template("tela-transferencia.html", titulo="Transferência")
+    return render_template("transferencia.html", titulo="Transferência")
 
 #Rota da página Extrato
 @app.route("/extrato", methods=["GET", "POST"])

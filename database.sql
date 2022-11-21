8cebad3395dfe804caeba18a8bdab55b634153c9
Jonas Rafael Siqueira Ribeiro <jonas.ribeiro01@fatec.sp.gov.br>
2022-11-21 19:23:40 -0300

    resolução de bugs relacionados ao Gerente Geral

--- database.sql
+++ database.sql
@@ -2,6 +2,44 @@ create database flaskapp;
 
 USE flaskapp;
 
+CREATE TABLE gerenteAgencia (
+  gerente_id INT NOT NULL AUTO_INCREMENT,
+  gerente_nome VARCHAR(45) NOT NULL,
+  gerente_cpf VARCHAR(11) NOT NULL,
+  gerente_nasc TEXT(6) NOT NULL,
+  gerente_genero VARCHAR(30) NOT NULL,
+  gerente_end VARCHAR(150) NOT NULL,
+  num_matricula VARCHAR(5) NOT NULL,
+  num_agencia text(10) NOT NULL,
+  data_criacao text(6) NOT NULL,
+  num_senha VARCHAR(180) NOT NULL,
+  PRIMARY KEY (gerente_id),
+  UNIQUE INDEX num_matricula_UNIQUE (num_matricula ASC));
+  
+
+CREATE TABLE agencias (
+  agencia_id INT PRIMARY KEY AUTO_INCREMENT,
+  numero_agencia VARCHAR(20),
+  numero_clientes VARCHAR(3),
+  data_criacao text(6) not null,
+  gerente_id int,
+  foreign key(gerente_id) references gerenteAgencia (gerente_id),
+  end_agencia VARCHAR (150));
+
+CREATE TABLE gerenteGeral (
+  GG_id INT NOT NULL primary key AUTO_INCREMENT,
+  GG_nome VARCHAR(45) NOT NULL,
+  GG_num_matricula VARCHAR(5) NOT NULL,
+  GG_num_senha VARCHAR(180) NOT NULL,
+  GG_primeira_vez tinyint(1) not null);
+  
+INSERT INTO gerenteGeral (
+GG_nome,
+GG_num_matricula,
+GG_num_senha,
+GG_primeira_vez)
+VALUES ('Gerente Geral', 00001, 123, 1);
+
 CREATE TABLE users (
   user_id INT PRIMARY KEY AUTO_INCREMENT,
   nome VARCHAR(40),
@@ -15,6 +53,8 @@ CREATE TABLE users (
   contaBancaria VARCHAR(9),
   agenciaBancaria VARCHAR(4),
   saldoBancario DECIMAL(19,2),
+  gerente_id int,
+  foreign key(gerente_id) references gerenteAgencia (gerente_id),
   UNIQUE INDEX contaBancaria_UNIQUE (contaBancaria ASC));
 
 create table movimentacaoConta
@@ -54,6 +94,23 @@ cpf VARCHAR(11) NOT NULL,
 dataAniversario TEXT(6) NOT NULL,
 genero VARCHAR(30) NOT NULL,
 endereco VARCHAR(150) NOT NULL,
+tipoConta VARCHAR(30),
+contaBancaria VARCHAR(9) NOT NULL,
+agenciaBancaria VARCHAR(4) NOT NULL,
+statusSolicitacao VARCHAR(40) not null,
+user_id int,
+foreign key(user_id) references users (user_id),
+solicitacao_id int,
+foreign key(solicitacao_id) references gerenciamentoUsuarios (solicitacao_id));
+
+create table confirmacaoFechamento
+(fechamento_id int primary key auto_increment,
+nome VARCHAR(40) NOT NULL,
+cpf VARCHAR(11) NOT NULL,
+dataAniversario TEXT(6) NOT NULL,
+genero VARCHAR(30) NOT NULL,
+endereco VARCHAR(150) NOT NULL,
+tipoConta VARCHAR(30),
 contaBancaria VARCHAR(9) NOT NULL,
 agenciaBancaria VARCHAR(4) NOT NULL,
 statusSolicitacao VARCHAR(40) not null,
@@ -77,41 +134,11 @@ foreign key(user_id) references users (user_id),
 solicitacao_id int,
 foreign key(solicitacao_id) references gerenciamentoUsuarios (solicitacao_id));
 
-CREATE TABLE gerenteAgencia (
-  gerente_id INT NOT NULL AUTO_INCREMENT,
-  gerente_nome VARCHAR(45) NOT NULL,
-  gerente_cpf VARCHAR(11) NOT NULL,
-  gerente_nasc TEXT(6) NOT NULL,
-  gerente_genero VARCHAR(30) NOT NULL,
-  gerente_end VARCHAR(150) NOT NULL,
-  num_matricula VARCHAR(5) NOT NULL,
-  num_agencia text(10) NOT NULL,
-  data_criacao text(6) NOT NULL,
-  num_senha VARCHAR(180) NOT NULL,
-  PRIMARY KEY (gerente_id),
-  UNIQUE INDEX num_matricula_UNIQUE (num_matricula ASC));
-  
-
-CREATE TABLE agencias (
-  agencia_id INT PRIMARY KEY AUTO_INCREMENT,
-  numero_agencia VARCHAR(20),
-  numero_clientes VARCHAR(3),
-  data_criacao text(6) not null,
-  end_agencia VARCHAR (150));
-
-CREATE TABLE gerenteGeral (
-  GG_id INT NOT NULL primary key AUTO_INCREMENT,
-  GG_nome VARCHAR(45) NOT NULL,
-  GG_num_matricula VARCHAR(5) NOT NULL,
-  GG_num_senha VARCHAR(180) NOT NULL,
-  GG_primeira_vez tinyint(1) not null);
-  
-INSERT INTO gerenteGeral (
-GG_nome,
-GG_num_matricula,
-GG_num_senha,
-GG_primeira_vez)
-VALUES ('Gerente Geral', 00001, 123, 1);
+create table configBanco(
+config_id int primary key auto_increment,
+capitalTotal decimal(19,2) not null,
+taxaJurosPoupanca decimal(4,4) not null,
+taxaJurosCheque decimal(4,4) not null);
 
 create table poupanca(
 poupanca_id int primary key auto_increment,
@@ -125,26 +152,23 @@ foreign key(user_id) references users (user_id),
 config_id int,
 foreign key(config_id) references configBanco (config_id));
 
-create table configBanco(
-config_id int primary key auto_increment,
-capitalTotal decimal(19,2) not null,
-taxaJurosPoupanca decimal(4,4) not null,
-taxaJurosCheque decimal(4,4) not null);
-
 select * from users;
 select * from movimentacaoConta;
 select * from gerenciamentoUsuarios;
 select * from confirmacaoDeposito;
 select * from confirmacaoAlteracao;
 select * from confirmacaoAbertura;
+select * from confirmacaoFechamento;
+
 select * from gerenteAgencia;
 select * from gerenteGeral;
 select * from agencias;
 select * from poupanca;
 select * from configBanco;
 
+SELECT g.dataHoraSolicitacao, g.tipoSolicitacao, g.usuarioDaSolicitacao, g.solicitacao_id FROM gerenciamentoUsuarios g, users u WHERE u.gerente_id = "1" and u.user_id = g.user_id;
 
-
+SELECT g.dataHoraSolicitacao, g.tipoSolicitacao, g.usuarioDaSolicitacao, g.solicitacao_id FROM gerenciamentoUsuarios g, users u WHERE u.gerente_id = "1" and u.user_id = g.user_id ORDER BY g.solicitacao_id DESC;
 
 
 
